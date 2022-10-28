8fc075c00dfdd181340ba09e7ef9eb1f1145429a
Sarah Watremet <sarah.watremet@gmail.com>
2022-10-28 14:39:00 +0200

    First round of organizing folders and files in the project

---
+++ AnimalPocket/AnimalPocket/Views/Items/GridItem.swift
@@ -0,0 +1,20 @@
+//
+//  GridItem.swift
+//  AnimalPocket
+//
+//  Created by Sarah Watremet on 28/10/2022.
+//
+
+import SwiftUI
+
+struct GridItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GridItem_Previews: PreviewProvider {
+    static var previews: some View {
+        GridItem()
+    }
+}
---
+++ AnimalPocket/AnimalPocket/Views/Items/GridView.swift
@@ -0,0 +1,20 @@
+//
+//  GridView.swift
+//  AnimalPocket
+//
+//  Created by Sarah Watremet on 28/10/2022.
+//
+
+import SwiftUI
+
+struct GridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GridView_Previews: PreviewProvider {
+    static var previews: some View {
+        GridView()
+    }
+}
