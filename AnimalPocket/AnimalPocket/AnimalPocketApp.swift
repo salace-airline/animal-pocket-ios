4093c3b681d89dc668b0ca1222a772adb820751c
Céline Barbe <laloutrecosmique@hotmail.fr>
2022-10-28 09:35:02 +0200

    first commit

---
+++ AnimalPocket/AnimalPocket/AnimalPocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalPocketApp.swift
+//  AnimalPocket
+//
+//  Created by Céline Barbe on 28/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimalPocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
