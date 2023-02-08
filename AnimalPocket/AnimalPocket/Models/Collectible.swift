024902b2ee6ddf954c66668ef2c06c5f381db44f
Sarah Watremet <sarah.watremet@gmail.com>
2023-02-08 16:46:50 +0100

    [#29] Removed useless init

--- AnimalPocket/AnimalPocket/Models/Collectible.swift
+++ AnimalPocket/AnimalPocket/Models/Collectible.swift
@@ -16,17 +16,6 @@ public struct Collectible: Identifiable {
   public let price: Int
   public let imageURI: String
   public let iconURI: String
-  
-  init(id: Int, name: Name, availability: Availability, speed: String?, shadow: String?, price: Int, imageURI: String, iconURI: String) {
-    self.id = id
-    self.name = name
-    self.availability = availability
-    self.speed = speed
-    self.shadow = shadow
-    self.price = price
-    self.imageURI = imageURI
-    self.iconURI = iconURI
-  }
 }
 
 extension Collectible: Decodable {
