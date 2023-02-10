4a979aa0dc7b06ba9760f8f3a338cd61036e554e
Sarah Watremet <sarah.watremet@gmail.com>
2023-02-10 15:58:43 +0100

    [#30] Price filters tests

--- AnimalPocket/AnimalPocketTests/AnimalPocketTests.swift
+++ AnimalPocket/AnimalPocketTests/AnimalPocketTests.swift
@@ -9,28 +9,38 @@ import XCTest
 @testable import AnimalPocket
 
 final class AnimalPocketTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+  var sut: BugsViewModel!
+  
+  override func setUpWithError() throws {
+    try super.setUpWithError()
+    sut = BugsViewModel()
+  }
+  
+  override func tearDownWithError() throws {
+    sut = nil
+    try super.tearDownWithError()
+  }
+  
+  func testDecreasingPrices() throws {
+    // given
+    let itemsArray = [Collectible.bugSample, Collectible.fishSample]
+    
+    // when
+    let filteredArray = sut.filter.decreasePrice(of: itemsArray)
+    
+    // then
+    XCTAssertTrue(filteredArray[0].price > filteredArray[1].price, "The prices have not been filtered correctly")
+  }
+  
+  func testIncreasingPrices() throws {
+    // given
+    let itemsArray = [Collectible.bugSample, Collectible.fishSample]
+    
+    // when
+    let filteredArray = sut.filter.increasePrice(of: itemsArray)
+    
+    // then
+    XCTAssertTrue(filteredArray[0].price < filteredArray[1].price, "The prices have not been filtered correctly")
+  }
+  
 }
